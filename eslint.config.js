956846ae40a2b0ce33d99eee0b0acc6a8f58354d
Mason Mason <mason.mason@yahooinc.com>
2025-09-07 01:58:38 -0400

    init

--- eslint.config.js
+++ eslint.config.js
@@ -7,4 +7,29 @@ module.exports = defineConfig([
   {
     ignores: ['dist/*'],
   },
+  {
+    files: ['**/__tests__/**/*', '**/__mocks__/**/*', '**/*.test.*', '**/*.spec.*'],
+    rules: {
+      '@typescript-eslint/no-require-imports': 'off',
+    },
+  },
+  {
+    // Additional rules to prevent scope/closure issues and improve code quality
+    rules: {
+      'no-use-before-define': 'off',
+      '@typescript-eslint/no-use-before-define': ['error', { 
+        functions: false, 
+        classes: true, 
+        variables: false,
+        ignoreTypeReferences: true
+      }],
+      'prefer-const': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'no-var': 'error',
+      'no-implicit-globals': 'error',
+    },
+  },
 ]);
