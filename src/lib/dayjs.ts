cdf6c539aabc1286bebf390e8f0a1129d8428b27
PhreeMason <mr.omarmason@gmail.com>
2025-11-06 23:07:41 -0500

    bugfix for dailygoal tracking

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -1,8 +1,10 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import utc from 'dayjs/plugin/utc';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 
 dayjs.extend(utc);
 dayjs.extend(relativeTime);
+dayjs.extend(isSameOrAfter);
 
 export { dayjs };
